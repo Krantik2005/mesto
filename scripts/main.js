892a3906515380757d1800e6fb438822d359dd58
Krantik2005 <krantik2005@yandex.ru>
2020-08-21 20:08:12 +0300

    проектная работа №5

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,98 @@
+//Массив cards
+const initialCards = [
+  {
+      name: 'Гора Бештау',
+      link: './images/cards/Beshtau.jpg'
+  },
+  {
+      name: 'Парк Железноводска',
+      link: './images/cards/Zheleznovodsk.jpg'
+  },
+  {
+      name: 'Зима в горах',
+      link: './images/cards/winter.jpg'
+  },
+  {
+      name: 'Гора Змейка',
+      link: './images/cards/Snake.jpg'
+  },
+  {
+      name: 'Монастырь у подножья горы',
+      link: './images/cards/Monastery.jpg'
+  },
+  {
+      name: 'Гора Эльбрус',
+      link: './images/cards/Elbrus.jpg'
+  }
+];
+
+//Объявляем переменные
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupClose = popup.querySelector('.popup__close');
-let nameInput = popup.querySelector('.text_name');
-let selfInput = popup.querySelector('.text_self');
-let title = document.querySelector('.profile__title');
-let subtitle = document.querySelector('.profile__subtitle');
+const popupMyForm = document.querySelector('.popup_my-form');
+const popupCloseMyForm = document.querySelector('.close_popup_myForm-close');
+const nameInput = popupMyForm.querySelector('.text_name');
+const selfInput = popupMyForm.querySelector('.text_self');
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+const popupCardForm = document.querySelector('.popup_card-form');
+const addButton = document.querySelector('.profile__add-button');
+const popupCloseCardForm = popupCardForm.querySelector('.close_popup_cardForm-close');
+const placeInput = popupCardForm.querySelector('.text_card-name');
+const imageInput = popupCardForm.querySelector('.text_card-img');
+const cardContainer = document.querySelector('.container');
 
-//Добавляет класс popup_opened и копирует данные (из h1 и p, в input)
-function popupAdd() {
-  nameInput.value = title.textContent;
-  selfInput.value = subtitle.textContent;
-  popup.classList.add('popup_opened');
+//функция добавления карточек
+const addCardToContainer = (card) => {
+  //клонируем
+  const cardElement = document.querySelector('#cardTemplate').content.cloneNode(true);
+
+  //добавляем картинки, текст и alt карточкам
+  const cardTemplateImage = cardElement.querySelector('.cards__image');
+  cardTemplateImage.src = card.link;
+  cardTemplateImage.alt = card.name;
+  
+  cardElement.querySelector('.cards__text').textContent = card.name;
+
+  //ставим убираем лайк
+  cardElement.querySelector('.cards__icon').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('cards__icon_active');
+  });
+
+  //открытие карточек при нажатии
+  const popupImageOpened = function() {
+    document.querySelector('.popup_card-image').classList.add('popup_cardImageOpened');
+    const popupImage = document.querySelector('.popup__img');
+    popupImage.src = card.link;
+    popupImage.alt = card.name;
+    document.querySelector('.popup__text').textContent = card.name;
+
+    //закрытие карточки при нажатии
+    document.querySelector('.close_popup_image-close').addEventListener('click', function() {
+        document.querySelector('.popup_card-image').classList.remove('popup_cardImageOpened');
+    });
+  }
+
+  cardElement.querySelector('.cards__image').addEventListener('click', popupImageOpened);
+
+  //удаление карточки при нажатии на мусорку
+  cardElement.querySelector('.cards__trash').addEventListener('click', function() {
+    deleteCard(event);
+  });
+
+  //отображаем на странице
+  cardContainer.prepend(cardElement);
 }
 
-//Удаляет класс popup_opened
-function popupRemove() {
-  popup.classList.remove('popup_opened');
+//ищем в массиве
+initialCards.forEach(addCardToContainer);
+
+// функция для удаления карточек
+function deleteCard(evt) {
+  evt.target.closest('.cards').remove();
 }
 
-//Обработчик отправки формы
+//Обработчик отправки формы submit
 function formSubmitHandler(evt) {
-  //Отмена стандартной отправки формы
   evt.preventDefault();
 
   //Добавляем текст на страницу
@@ -28,10 +100,64 @@ function formSubmitHandler(evt) {
   subtitle.textContent = selfInput.value;
 
   //Закрываем попап
-  popupRemove();
+  popupMyFormRemove();
 }
 
+  //Добавляем новую карточку
+  function formSubmitCard(evt) {
+    evt.preventDefault();
+
+    if (placeInput.value === '' || imageInput.value === '') {
+      alert('Заполните все поля');
+      return false;
+    }
+
+    const addCard = {
+      name: placeInput.value,
+      link: imageInput.value
+  }
+  
+    addCardToContainer(addCard);
+
+    popupCardFormRemove();
+  } 
+
+//Добавляет класс popup_opened и копирует данные (из h1 и p, в input)
+function popupMyFormAdd() {
+  nameInput.value = title.textContent;
+  selfInput.value = subtitle.textContent;
+  popupMyForm.classList.add('popup_myFormOpened');
+}
+
+//Удаляет класс popup_opened
+function popupMyFormRemove() {
+  popupMyForm.classList.remove('popup_myFormOpened');
+}
+
+
+//Открытие закрытие popup карточек
+function popupCardFormAdd() {
+  popupCardForm.classList.add('popup_cardFormOpened');
+}
+
+function popupCardFormRemove() {
+  popupCardForm.classList.remove('popup_cardFormOpened');
+}
+
+
 //Вызываем обработчики событий
-popup.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener('click', popupAdd);
-popupClose.addEventListener('click', popupRemove);
+popupMyForm.addEventListener('submit', formSubmitHandler);
+popupCardForm.addEventListener('submit', formSubmitCard);
+
+editButton.addEventListener('click', popupMyFormAdd);
+popupCloseMyForm.addEventListener('click', popupMyFormRemove);
+addButton.addEventListener('click', popupCardFormAdd);
+popupCloseCardForm.addEventListener('click', popupCardFormRemove);
+
+
+
+
+
+
+
+
